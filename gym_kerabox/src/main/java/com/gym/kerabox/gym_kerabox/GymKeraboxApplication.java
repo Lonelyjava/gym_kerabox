a5a9e57d217f3644380b4ec31d0f108be10e6754
Lonelyjava <kunk8667@gmail.com>
2023-02-19 19:58:08 +0530

    code commit for gym project

---
+++ gym_kerabox/src/main/java/com/gym/kerabox/gym_kerabox/GymKeraboxApplication.java
@@ -0,0 +1,13 @@
+package com.gym.kerabox.gym_kerabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymKeraboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymKeraboxApplication.class, args);
+	}
+
+}
---
+++ gym_kerabox/src/test/java/com/gym/kerabox/gym_kerabox/GymKeraboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.kerabox.gym_kerabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymKeraboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
