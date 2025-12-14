e67bd2ff4c7268daa373e37a1aec9a0657342996
Lonelyjava <kumarkundan784@gmail.com>
2025-12-14 12:03:17 +0530

    update resttemplate

--- gym_kerabox/src/main/java/com/gym/kerabox/GymKeraboxApplication.java
+++ gym_kerabox/src/main/java/com/gym/kerabox/GymKeraboxApplication.java
@@ -1,15 +1,26 @@
 package com.gym.kerabox;
 
+/*Developer : Kundan Kumar
+ * date : 14-12-2025
+ * Project - Gym Management
+ * 
+ * 
+ * */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GymKeraboxApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(GymKeraboxApplication.class, args);
 	}
 
-	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
